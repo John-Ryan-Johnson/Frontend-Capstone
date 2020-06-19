14941eb656352818db217bf52ae9f10111f1ca36
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-06-19 16:41:19 -0500

    added paths for crud on motorcycles to App.js

--- src/App/App.js
+++ src/App/App.js
@@ -2,19 +2,40 @@ import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+
 import './App.scss';
 
 import MyNavbar from '../components/shared/MyNavbar/MyNavbar';
 
 import Auth from '../components/pages/Auth/Auth';
 import EditMotorcycle from '../components/pages/EditMotorcycle/EditMotorcycle';
-import Motorcycle from '../components/pages/Motorcycle/Motorcycle';
+import Motorcycles from '../components/pages/Motorcycles/Motorcycles';
 import NewMotorcycle from '../components/pages/NewMotorcycle/NewMotorcycle';
 import SingleMotorcycle from '../components/pages/SingleMotorcycle/SingleMotorcycle';
 
 import fbConnection from '../helpers/data/connection';
 
 fbConnection();
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/motorcycles', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
 
 class App extends React.Component {
   state = {
@@ -36,15 +57,32 @@ class App extends React.Component {
   }
 
   render() {
+    const { authed } = this.state;
     return (
       <div className="App">
-        <MyNavbar />
-        <h1>MotoWorx</h1>
-        <Auth />
-        <EditMotorcycle/>
-        <Motorcycle/>
-        <NewMotorcycle/>
-        <SingleMotorcycle/>
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavbar />
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <PrivateRoute path='/motorcycles/edit/:motorcycleId' component={EditMotorcycle} authed={authed} />
+                  <PrivateRoute path='/motorcycles/motorcycles/:motorcycleId' component={SingleMotorcycle} authed={authed} />
+                  <PrivateRoute path='/motorcycles/new' component={NewMotorcycle} authed={authed} />
+                  <PrivateRoute path='/motorcycles' component={Motorcycles} authed={authed} />
+                  <PrivateRoute path='/repairs/edit/:repairId' component={EditRepair} authed={authed}/>
+                  <PrivateRoute path='/repairs/new' component={NewRepair} authed={authed}/>
+                  <PrivateRoute path='/repairs/:repairId' component={SingleRepair} authed={authed}/>
+                  <PrivateRoute path='/repairs' component={Repairs} authed={authed}/>
+                  <PrivateRoute path='/mods/:repairId' component={SingleMod} authed={authed}/>
+                  <PrivateRoute path='/mods' component={Mods} authed={authed}/>
+                  <PublicRoute path='/auth' component={Auth} authed={authed} />
+                  <Redirect from="*" to="/motorcycles"/>
+                </Switch>
+              </div>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/components/pages/Motorcycles/Motorcycles.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Link } from 'react-dom';
+
+import './Motorcycles.scss';
+
+class Motorcycles extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Motorcycle Container</h1>
+      </div>
+    );
+  }
+}
+
+export default Motorcycles;
