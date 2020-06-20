e3b3d26695b46a588a5ef419074c87b97f8835b5
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-06-20 10:16:14 -0500

    motorcycle cards wip

--- src/components/pages/Motorcycles/Motorcycles.js
+++ src/components/pages/Motorcycles/Motorcycles.js
@@ -2,22 +2,39 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import authData from '../../../helpers/data/authData';
+import motorcyclesData from '../../../helpers/data/motorcyclesData';
+
 import './Motorcycles.scss';
 
 class Motorcycles extends React.Component {
-  editMotorcycleEvent = (e) => {
-    e.preventDefault();
-    const motorcycleId = 'monkeyButt25';
-    this.props.history.push(`/motorcycles/edit/${motorcycleId}`);
+  state = {
+    motorcycles: [],
+  }
+
+  getMotorcycles = () => {
+    const uid = authData.getUid();
+    motorcyclesData.getMotorcyclesByUid(uid)
+      .then((motorcycles) => this.setState({ motorcycles }))
+      .catch((err) => console.error('unable to get motorcycles:', err));
+  }
+
+  componentDidMount() {
+    this.getMotorcycles();
   }
 
   render() {
+    const { motorcycles } = this.state;
+    const buildMotorcycleCards = motorcycles.map((motorcycle) => (
+      <MotorcycleCard key={motorcycle.id} motorcycle={motorcycle}/>
+    ));
     return (
       <div>
-        <h1>Motorcycles</h1>
-        <button className="btn btn-dark" onClick={this.editMotorcycleEvent}>Edit Motorcycle</button>
-        <Link to='/motorcycles/motorcycles/motorcycle1'>View Single Motorcycle</Link>
-        <Link to='/motorcycles/new'>New Motorcycle</Link>
+        <h1>My Motorcycles</h1>
+        <Link to='/motorcycles/new'><i className="fas fa-plus"></i></Link>
+        <div className="d-flex flex-wrap">
+          {buildMotorcycleCards}
+        </div>
       </div>
     );
   }
