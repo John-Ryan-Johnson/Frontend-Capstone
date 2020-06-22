48188726e1d363344d5d4254640aa9cdb7d06f80
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-06-22 11:11:18 -0500

    can create new motorcycle

--- src/components/pages/NewMotorcycle/NewMotorcycle.js
+++ src/components/pages/NewMotorcycle/NewMotorcycle.js
@@ -1,11 +1,93 @@
 import React from 'react';
+
+import authData from '../../../helpers/data/authData';
+import motorcyclesData from '../../../helpers/data/motorcyclesData';
+
 import './NewMotorcycle.scss';
 
 class NewMotorcycle extends React.Component {
+  state = {
+    motorcycleName: '',
+    motorcycleImageUrl: '',
+    motorcycleMileage: 0,
+  }
+
+  nameChange = (e) => {
+    e.preventDefault();
+    this.setState({ motorcycleName: e.target.value });
+  }
+
+  imageUrlChange = (e) => {
+    e.preventDefault();
+    this.setState({ motorcycleImageUrl: e.target.value });
+  }
+
+  mileageChange = (e) => {
+    e.preventDefault();
+    this.setState({ motorcycleMileage: e.target.value });
+  }
+
+  saveMotorcycle = (e) => {
+    e.preventDefault();
+    const {
+      motorcycleName,
+      motorcycleImageUrl,
+      motorcycleMileage,
+    } = this.state;
+    const newMotorcycle = {
+      name: motorcycleName,
+      imageUrl: motorcycleImageUrl,
+      mileage: motorcycleMileage,
+      uid: authData.getUid(),
+    };
+    motorcyclesData.postMotorcycle(newMotorcycle)
+      .then(() => this.props.history.push('/motorcycles'))
+      .catch((err) => console.error('unable to save motorcycle:', err));
+  }
+
   render() {
+    const {
+      motorcycleName,
+      motorcycleImageUrl,
+      motorcycleMileage,
+    } = this.state;
+
     return (
-      <div className="NewMotorcycle">
-        <h1>New Motorcycle</h1>
+      <div className="NewMotorcycle col-12">
+        <h1 className="form mt-5 mb-5">Motorcycle Form</h1>
+        <form className="col-6 offset-3">
+          <div className="form-group">
+            <label htmlFor="motorcycle-name">Name</label>
+            <input
+             type="text"
+             className="form-control"
+             id="motorcycle-name"
+             value={motorcycleName}
+             onChange={this.nameChange}
+             />
+          </div>
+          <div className="form-group">
+            <label htmlFor="motorcycle-imageUrl">Image Url</label>
+            <input
+             type="text"
+             className="form-control"
+             id="motorcycle-imageUrl"
+             value={motorcycleImageUrl}
+             onChange={this.imageUrlChange}
+             />
+          </div>
+          <div className="form-group">
+            <label htmlFor="motorcycle-mileage">Mileage</label>
+            <input
+             type="number"
+             className="form-control"
+             id="motorcycle-mileage"
+             value={motorcycleMileage}
+             onChange={this.mileageChange}
+             />
+          </div>
+          <button className="btn saveBtn mt-3" onClick={this.saveMotorcycle}>Save</button>
+        </form>
       </div>
     );
   }
