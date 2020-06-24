16554c5b957bd11fbf1aace6d0c969d17be95dd2
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-06-24 12:47:59 -0500

    styling of repair card WIP

--- src/components/shared/RepairCard/RepairCard.js
+++ src/components/shared/RepairCard/RepairCard.js
@@ -7,22 +7,40 @@ import './RepairCard.scss';
 class RepairCard extends React.Component {
   static propTypes = {
     repair: repairShape.repairShape,
-  }
+  };
 
   render() {
     const { repair } = this.props;
     const singleRepairLink = `/repairs/${repair.id}`;
     return (
-      <div className="RepairCard col-4">
-        <div className="card repairCard">
-          <div className="card-body">
-            <h3 className="card-title repairName">{repair.name}</h3>
-            <div>
-            <Link className="singleRepairView" style={{ textDecoration: 'none' }} to={singleRepairLink}>View</Link>
+      <div className='container'>
+        <div className='row justify-content-center'>
+          <div className='RepairCard col-4'>
+            <div className='card repairCard card-flip'>
+              <div className='card-front'>
+                <div className='card-body'>
+                  <h3 className='card-title repairName'>{repair.name}</h3>
+                </div>
+              </div>
+              <div className='card-back'>
+                <div className='card-body'>
+                  <div className="col-12">
+                    <div className="comment-container">
+                      <h5>Comments</h5>
+                      <div className="repairComments">{repair.comments}</div>
+                    </div>
+                    <div className="mileage-container">
+                      <h5 className="mt-2">Mileage</h5>
+                      <div className="repairMileage mt-3">{repair.mileage}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
     );
   }
 }
